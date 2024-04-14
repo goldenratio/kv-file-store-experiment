a850e0abb0419447b48b5af6234668637fef4416
goldenratio <karthikeyanvj@gmail.com>
2024-04-14 19:09:43 +0300

    init

--- src/index.ts
+++ src/index.ts
@@ -3,9 +3,13 @@ import { config } from './config.js';
 import { getRandomCharacter, randomInt } from './utils/math-utils.js';
 
 async function main(): Promise<void> {
-  await runTest('simulation-01', 100, true);
-  // await runTest('simulation-02', 15_000);
-  // await runTest('simulation-03', 100_000);
+  await runTest('simulation-01', 5_000);
+  await runTest('simulation-02', 15_000);
+  await runTest('simulation-03', 100_000);
+
+  await runTest('simulation-key-expiry-01', 5_000, true);
+  await runTest('simulation-key-expiry-02', 15_000, true);
+  await runTest('simulation-key-expiry-03', 100_000, true);
 }
 
 async function runTest(testName: string, iterations: number, keyExpiryTimeEnabled: boolean = false): Promise<void> {
